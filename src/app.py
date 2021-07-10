773ab8a19987f932bbd01e2d24f2f9f5b6a09ca1
vu_29 <vibhu.upamanyu@gmail.com>
2021-07-11 01:07:42 +0530

    jinja iteration created

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-
+import json
 
 app = Flask(__name__)
 
@@ -21,9 +21,38 @@ def pre_training():
 @app.route('/post_training', methods = ['GET','POST'] )
 def post_training():
     if request.method == 'POST':
+        return request.form
         return render_template('post_training.html')
-        
-    return render_template('post_training.html')
+
+    dic = {
+    "Gender" : {
+        "Male" : 1,
+        "Female" : 2
+    },
+    "Type of Disability": {"None": 1,
+        "Deaf": 2,
+        "Locomotor Disability": 3,
+        "Low-vision": 4,
+        "Blindness (Visually Impaired)": 6,
+        "Leprosy Cured Person": 7,
+        "Hard of Hearing": 8,
+        "Speech and Language Disability (Speech Impaired)": 9,
+        "Mental Behavior- Mental Illness, Mental Retardation": 10,
+        "Cerebral Palsy": 11,
+        "Muscular Dystrophy": 12,
+        "Dwarfism": 13
+    },
+    "castecategory": {"Gen": 1,
+    "SC": 2,
+    "OBC": 3,
+    "ST": 4
+    }
+}
+
+    
+
+
+    return render_template('post_training.html',category = dic)
 
 
 if __name__ == "__main__":
