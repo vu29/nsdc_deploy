6d5dddf5b53074000e1147bd4fc8801c893d79f3
vu_29 <vibhu.upamanyu@gmail.com>
2021-07-19 16:40:06 +0530

    prediction route setup

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
-from datetime import timedelta
 import json
+import string
+import random
 from flask import Flask, render_template, request
-from utils import get_prediction_without_result,get_prediction_with_result
+from flask.globals import session
+from pandas.core.arrays import categorical
+from pandas.core.indexes import category
+from utils import *
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
 # select values
 pre_train_select = json.load(
@@ -35,6 +41,58 @@ def post_training():
 
     return render_template('post_training.html', title="Post-Training", category=post_train_select)
 
+
+@app.route('/predict/user_info', methods=['GET','POST'])
+def predict_user_info():
+    if request.method == 'POST':
+        return request.form
+    
+    return render_template('predict_1.html',title = 'Predict',category = post_train_select)
+
+@app.route('/predict/tc_name', methods = ['POST'])
+def tc_name():
+    dist_code = request.form['TC District']
+    session['user_info'] = request.form.to_dict()
+    print(session)
+    category = dict()
+    category['TC Name'] = get_tc(dist_code)
+    return render_template('predict_2.html',title = 'Predict',category = category, next_url = '/predict/partner_name')
+
+@app.route('/predict/partner_name', methods = ['POST'])
+def parter_name():
+    session['TC Name'] = request.form['TC Name']
+    print(session)
+    category = dict()
+    category['Partner Name'] = get_partner(session['user_info']['TC District'],session['TC Name'])
+    return render_template('predict_2.html',title = 'Predict',category = category, next_url = '/predict/sector_name')
+
+@app.route('/predict/sector_name', methods = ['POST'])
+def sector_name():
+    session['PartnerName'] = request.form['Partner Name']
+    print(session)
+    category = dict()
+    category['Sector Name'] = get_sector(session['user_info']['TC District'],session['TC Name'],session['PartnerName'])
+    return render_template('predict_2.html',title = 'Predict',category = category, next_url = '/predict/job_role')
+    
+@app.route('/predict/job_role', methods = ['POST'])
+def job_role():
+    session['SectorName'] = request.form['Sector Name']
+    print(session)
+    category = dict()
+    category['Job Role'] = get_job_role(session['user_info']['TC District'],session['TC Name'],session['PartnerName'],session['SectorName'])
+    return render_template('predict_2.html',title = 'Predict',category = category, next_url = '/predict/result')
+
+@app.route('/predict/result', methods = ['POST'])
+def predict_result():
+    final_dict = {}
+    for key in dict(session).keys():
+        if(key == 'user_info'):
+            final_dict.update(session[key])
+        else:
+            final_dict[key] = session[key]
+    final_dict.update(request.form)
+    print(final_dict)
+    return str(get_prediction(final_dict))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,8 @@ def format_dict(input_dict):
     name_correction_dict = {
         'Age Group': 'agegroup',
         'Caste Category': 'castecategory',
-        'Type of Disability': 'TypeofDisability'
+        'Type of Disability': 'TypeofDisability',
+        'Job Role' : 'JobRole'
 
     }
     new_dict = dict()
@@ -52,7 +53,7 @@ def get_prediction_without_result(form_data,):
     return data
 
 
-def get_prediction_with_result(form_data):
+def get_prediction_with_result(form_data,only_result = False):
     final_col_order = ['Gender', 'TypeofDisability', 'castecategory', 'Religion', 'JobRole', 'SectorName', 'PartnerName', 'TC State', 'TC District', 'TC Name', 'TotalCandidatesinBatch', 'Result', 'Grade', 'Certified', 'agegroup', 'EducationLevel','No. of colleges', 'Trade Count', 'No of ITI', 'Ph.D', 'M.Phil', 'Post Graduate', 'Under Graduate', 'PG Diploma', 'Diploma', 'Certificate', 'Integrated', 'Total Number of Schools', 'Total Poulation', 'Sexratio', 'Decadal Growth Rate', 'Overall Literacy', 'Female Literacy', 'Primary Enrollment', 'Primary with Upper Primary Enrollment', 'Primary with Upper Primary sec/higher sec. Enrollment', 'Upper Primary Only Enrollment', 'Upper Primary with sec./higher sec. Enrollment', 'Primary Teacher', 'Primary with Upper Primary Teacher', 'Primary with Upper Primary sec/higher sec. Teacher', 'Upper Primary Only Teacher', 'Upper Primary with sec./higher sec. Teacher', 'number of Industry total', 'Business Services', 'Manufacturing (Food stuffs)', 'Real Estate and Renting', 'Trading', 'Community, personal & Social Services', 'Agriculture and Allied Activities', 'Construction', 'Manufacturing (Textiles)', 'Transport, storage and Communications', 'Electricity, Gas & Water companies', 'Manufacturing (Paper & Paper products, Publishing, printing and reproduction of recorded media)', 'Manufacturing (Machinery & Equipments)', 'Manufacturing (Others)', 'Finance', 'Manufacturing (Metals & Chemicals, and products thereof)', 'Manufacturing (Leather & products thereof)', 'Mining & Quarrying', 'Manufacturing (Wood Products)', 'Insurance', 'Manufacturing (Paper & Paper products, Publishing, PRIVnting and reproduction of recorded media)', 'Total population', 'Population Age(15-21)', 'Population (15-24) Absolute', 'Population (5-14) Absolute', 'Population Total male', 'Population Total female', 'Population Hindu', 'Population Muslim', 'Population christian', 'Population Sikh', 'Population Jain', 'Population other', 'Total Households', 'Total Rural Household', 'Total Urban Household', 'Number Normal Rural', 'Number Normal Urban', 'Institutional Rural', 'Institutional Urban', 'Houseless Rural', 'Houseless Urban', 'Houseless with Shelter Urban', 'Houseless without Shelter Urban', 'Total Population', 'No of Population Having Highest Education Level of Illitrate', 'No of Population Having Highest Education Level of Llitrate But Below Primary', 'No of Population Having Highest Education Level of Primary', 'No of Population Having Highest Education Level of Middle', 'No of Population Having Highest Education Level of Secondary', 'No of Population Having Highest Education Level of Higher Secondary', 'No of Population Having Highest Education Level of Graduate Or Higher', 'Not In Labour Force', 'Employed', 'Unemployed']
     form_data = format_dict(form_data)
     dist_code = form_data['TC District']
@@ -72,6 +73,8 @@ def get_prediction_with_result(form_data):
     X = df[final_col_order].values
     model = pickle.load(open('./models/model_withresult_external.sav', 'rb'))
     prediction = model.predict(X).tolist()
+    if only_result:
+        return prediction
     table_df = df[['TC Name','PartnerName','SectorName','JobRole']]
     nsdc_json = json.load(open('./models/final_data/dictnsdc.json','r'))
     for col in table_df.columns:
@@ -81,3 +84,70 @@ def get_prediction_with_result(form_data):
     for i in range(len(prediction)):
         data[i]['Result'] = prediction[i]
     return data
+
+
+def get_prediction(form_data):
+    final_col_order = ['Gender', 'TypeofDisability', 'castecategory', 'Religion', 'JobRole', 'SectorName', 'PartnerName', 'TC State', 'TC District', 'TC Name', 'TotalCandidatesinBatch', 'Result', 'Grade', 'Certified', 'agegroup', 'EducationLevel','No. of colleges', 'Trade Count', 'No of ITI', 'Ph.D', 'M.Phil', 'Post Graduate', 'Under Graduate', 'PG Diploma', 'Diploma', 'Certificate', 'Integrated', 'Total Number of Schools', 'Total Poulation', 'Sexratio', 'Decadal Growth Rate', 'Overall Literacy', 'Female Literacy', 'Primary Enrollment', 'Primary with Upper Primary Enrollment', 'Primary with Upper Primary sec/higher sec. Enrollment', 'Upper Primary Only Enrollment', 'Upper Primary with sec./higher sec. Enrollment', 'Primary Teacher', 'Primary with Upper Primary Teacher', 'Primary with Upper Primary sec/higher sec. Teacher', 'Upper Primary Only Teacher', 'Upper Primary with sec./higher sec. Teacher', 'number of Industry total', 'Business Services', 'Manufacturing (Food stuffs)', 'Real Estate and Renting', 'Trading', 'Community, personal & Social Services', 'Agriculture and Allied Activities', 'Construction', 'Manufacturing (Textiles)', 'Transport, storage and Communications', 'Electricity, Gas & Water companies', 'Manufacturing (Paper & Paper products, Publishing, printing and reproduction of recorded media)', 'Manufacturing (Machinery & Equipments)', 'Manufacturing (Others)', 'Finance', 'Manufacturing (Metals & Chemicals, and products thereof)', 'Manufacturing (Leather & products thereof)', 'Mining & Quarrying', 'Manufacturing (Wood Products)', 'Insurance', 'Manufacturing (Paper & Paper products, Publishing, PRIVnting and reproduction of recorded media)', 'Total population', 'Population Age(15-21)', 'Population (15-24) Absolute', 'Population (5-14) Absolute', 'Population Total male', 'Population Total female', 'Population Hindu', 'Population Muslim', 'Population christian', 'Population Sikh', 'Population Jain', 'Population other', 'Total Households', 'Total Rural Household', 'Total Urban Household', 'Number Normal Rural', 'Number Normal Urban', 'Institutional Rural', 'Institutional Urban', 'Houseless Rural', 'Houseless Urban', 'Houseless with Shelter Urban', 'Houseless without Shelter Urban', 'Total Population', 'No of Population Having Highest Education Level of Illitrate', 'No of Population Having Highest Education Level of Llitrate But Below Primary', 'No of Population Having Highest Education Level of Primary', 'No of Population Having Highest Education Level of Middle', 'No of Population Having Highest Education Level of Secondary', 'No of Population Having Highest Education Level of Higher Secondary', 'No of Population Having Highest Education Level of Graduate Or Higher', 'Not In Labour Force', 'Employed', 'Unemployed']
+    form_data = format_dict(form_data)
+    dist_code = form_data['TC District']
+    
+    ## state code
+    state_dict = json.load(open('./models/final_data/dist_state_code.json'))
+    state_code = state_dict[str(dist_code)]
+    
+
+    dist_df = pd.read_csv('./models/final_data/final_dist_normalized.csv')
+    dist_df = dist_df[dist_df['District'] == dist_code].iloc[:,1:]
+    dist_df['TC State'] = int(state_code)
+    for key in form_data:
+        dist_df[key] = form_data[key]
+    X = dist_df[final_col_order].values
+    model = pickle.load(open('./models/model_withresult_external.sav', 'rb'))
+    prediction = model.predict(X).tolist()
+    return prediction
+
+
+
+def get_tc(dist):
+    df = pd.DataFrame.from_dict(json.load(open('./models/final_data/tc_sector_partner_jobrole_with_code.json'))[str(dist)])
+    nsdc_dict = json.load(open('./models/final_data/dictnsdc.json'))
+    tc_list = df['TC Name'].unique().tolist()
+    col = 'TC Name'
+    inv_dict= dict(zip(nsdc_dict[col].values(),nsdc_dict[col].keys()))
+    req_dict = {}
+    for code in tc_list:
+        req_dict[inv_dict[code]] = code
+    return req_dict
+
+def get_partner(dist,tc):
+    df = pd.DataFrame.from_dict(json.load(open('./models/final_data/tc_sector_partner_jobrole_with_code.json'))[str(dist)])
+    nsdc_dict = json.load(open('./models/final_data/dictnsdc.json'))
+    col = 'PartnerName'
+    partner_list = df[df['TC Name'] == int(tc)][col].unique().tolist()
+    inv_dict= dict(zip(nsdc_dict[col].values(),nsdc_dict[col].keys()))
+    req_dict = {}
+    for code in partner_list:
+        req_dict[inv_dict[code]] = code
+    return req_dict
+
+def get_sector(dist,tc,partner):
+    df = pd.DataFrame.from_dict(json.load(open('./models/final_data/tc_sector_partner_jobrole_with_code.json'))[str(dist)])
+    nsdc_dict = json.load(open('./models/final_data/dictnsdc.json'))
+    col = 'SectorName'
+    partner_list = df[(df['TC Name'] == int(tc)) & (df['PartnerName'] == int(partner))][col].unique().tolist()
+    inv_dict= dict(zip(nsdc_dict[col].values(),nsdc_dict[col].keys()))
+    req_dict = {}
+    for code in partner_list:
+        req_dict[inv_dict[code]] = code
+    return req_dict
+
+def get_job_role(dist,tc,partner,sector):
+    df = pd.DataFrame.from_dict(json.load(open('./models/final_data/tc_sector_partner_jobrole_with_code.json'))[str(dist)])
+    nsdc_dict = json.load(open('./models/final_data/dictnsdc.json'))
+    col = 'JobRole'
+    partner_list = df[(df['TC Name'] == int(tc)) & (df['SectorName'] == int(sector)) & (df['PartnerName'] == int(partner))][col].unique().tolist()
+    inv_dict= dict(zip(nsdc_dict[col].values(),nsdc_dict[col].keys()))
+    req_dict = {}
+    for code in partner_list:
+        req_dict[inv_dict[code]] = code
+    return req_dict
